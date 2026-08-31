9410e0b545efa57d91344002cb76939dccef39bf
brauck <brauckhoff@yandex.com>
2026-08-31 03:48:42 +0300

    set reverb

--- chirper/resources/js/app.js
+++ chirper/resources/js/app.js
@@ -22,3 +22,10 @@
 //         submitBtn.disabled = false;
 //     }
 // }
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
